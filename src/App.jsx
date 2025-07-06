11735f21b9f18fa9052ab07f8ade63693cbf38c3
Jedyokey <jedyokey29@gmail.com>
2025-07-06 15:03:51 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+// import Home from './pages/Home'
+// import About from './pages/About'
+
+const App = () => {
+  return (
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/about" element={<About />} />
+    // </Routes>
+    <div className="app">
+      <h1>React Docs</h1>
+      <p>Welcome to the React documentation app!</p>
+      <p>Use the navigation links to explore different sections.</p>
+      {/* Add your routes and components here */}
+    </div>
+  )
+}
+
+export default App
